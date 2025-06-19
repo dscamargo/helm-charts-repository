3d4f50173dde3a1659d07d224f36c6930c0a267a
Douglas Camargo <douglas.camargo1010@gmail.com>
2025-06-19 16:02:09 -0300

    release base-deployment 0.0.1

--- create-cronjob-release.sh
+++ create-cronjob-release.sh
@@ -4,4 +4,4 @@ helm package charts/dscamargo-cronjob
 
 helm repo index --url https://dscamargo.github.io/helm-charts-repository --merge index.yaml .
 
-echo "Deployment release created successfully."
+echo "Cronjob release created successfully."
--- create-deployment-release.sh
+++ create-deployment-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm package charts/dscamargo-deployment
+helm package charts/base-deployment
 
 helm repo index --url https://dscamargo.github.io/helm-charts-repository --merge index.yaml .
 
